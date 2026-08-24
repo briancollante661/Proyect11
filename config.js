9b137cebbe2ed4c8b6ddfe803673bdd3dd4891a5
briancollante661 <briancollante661@email.com>
2026-08-24 10:12:18 -0300

    082426

--- config.js
+++ config.js
@@ -6,8 +6,7 @@ const SHEETS_CONFIG = {
 
   avisos: {
     spreadsheetId: 
-    "1aYJ0ZukO-DxzZN77sWMgmzhvsHRQXfsjdDUXel_jE2A",
-    //"1FAIpQLSfGnti73ZsA0ur7KM5S5QItflirTriVEWEo9NYMr3alnxEb6A",
+    "1xt5ep0wThwCWtr9Xz3y5pliaPhboC8bbfiTFrDIyxsc",
     hoja: "Avisos"
    },
 
@@ -17,7 +16,7 @@ const SHEETS_CONFIG = {
   },
 
   horarios: {
-    spreadsheetId: "1xt5ep0wThwCWtr9Xz3y5pliaPhboC8bbfiTFrDIyxsc",
+    spreadsheetId: "1aYJ0ZukO-DxzZN77sWMgmzhvsHRQXfsjdDUXel_jE2A",
     hojas: {
       primero: "Primero",
       segundo: "Segundo",
